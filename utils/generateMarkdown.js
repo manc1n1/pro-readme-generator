2adceea166c2ab40c6682facf4f4b798a08c84e4
manc1n1 <mancinij1111@gmail.com>
2023-08-03 17:00:04 -0400

    Render title and license in markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data, license) {
+	const licenseBadge = `https://img.shields.io/github/license/${data.github}/${data.title}.svg?style=for-the-badge`;
+
+	if (license !== 'None') return licenseBadge;
+	else return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data, license) {
+	const licenseLink = `https://github.com/${data.github}/${data.title}/blob/${data.branch}/LICENSE`;
+
+	if (license !== 'None') return licenseLink;
+	else return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data, license) {
+	const licenseRender = `
+  ## Badges
+  [![License](${renderLicenseBadge(data, license)})](${renderLicenseLink(
+		data,
+		license,
+	)})
+  `;
+
+	if (license !== 'None') return licenseRender;
+	else return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+	return `# ${data.title}
+  ${renderLicenseSection(data, data.license)}
 `;
 }
 
